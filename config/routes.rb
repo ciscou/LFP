57ea1e6baa2dc149a275542450fd0568d97d879b
ciscou <francismpp@gmail.com>
2011-04-28 20:45:27 +0200

    Installed nifty-generators

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Lfp::Application.routes.draw do
+  resources :sessions
+  resources :users
+
+  match 'user/edit' => 'users#edit', :as => :edit_current_user
+  match 'signup' => 'users#new', :as => :signup
+  match 'logout' => 'sessions#destroy', :as => :logout
+  match 'login' => 'sessions#new', :as => :login
+
+  root :to => "users#new"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
