fa8664538e955b7dcf2abccdb13b5c990f725ad9
ciscou <francismpp@gmail.com>
2011-04-28 20:17:40 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lfp::Application.config.secret_token = '5401f5dea5e7cbf4c31ddd8e71d5ca2693f88ed548ea4800a9e544b7e867f6edcff64877bfc9f53087bd3fca4623855d99991c88dff6164f5592294ec64f2191'
